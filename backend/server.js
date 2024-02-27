ccbcb2de0a870ffa8292a5e1e3e00584cf5734d6
Anton Dobrynin <andeedobro@gmail.com>
2024-02-27 23:26:42 +0500

    init

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+import Fastify from 'fastify';
+import PostgresFastify from '@fastify/postgres';
+
+const fastify = Fastify({
+    logger: true
+});
+
+
+// Route to get all posts
+fastify.get('/posts', async (request, reply) => {
+    fastify.pg.query(
+        'SELECT * FROM posts',
+        function onResult(err, result) {
+            reply.send(err || result)
+        }
+    );
+});
+
+// Test route
+fastify.get('/api', async (request, reply) => {
+    reply
+    .code(200)
+    .header('Content-Type', 'application/json; charset=utf-8')
+    .send({ hello: 'world' })
+});
+
+// Route to create a new post
+fastify.post('/posts', async (request, reply) => {
+    const { text } = request.body;
+
+    fastify.pg.query(
+        'INSERT INTO posts (text) VALUES ($1) RETURNING *', [text],
+        function onResult(err, result) {
+            reply.send(err || result)
+        }
+    )
+});
+
+
+async function createTable() {
+    const client = await fastify.pg.connect();
+
+    // Execute SQL query to create the posts table
+    await client.query(
+        `CREATE TABLE IF NOT EXISTS posts (id SERIAL PRIMARY KEY, text TEXT);`
+    );
+
+    client.release();
+}
+
+function start() {
+    fastify.register(PostgresFastify, {
+        connectionString: 'postgresql://user:example@db:5432/database',
+        connectionTimeoutMillis: 10_000,
+    })
+        .then(() => {
+            return createTable();
+        })
+        .then(() => {
+            fastify.listen({ port: 3000 }, function (err, address) {
+                if (err) {
+                    fastify.log.error(err)
+                    process.exit(1)
+                }
+            });
+        })
+}
+
+start();
---
+++ client/src/App.tsx
@@ -0,0 +1,44 @@
+import { createResource, type Component, createSignal, For } from 'solid-js';
+
+import styles from './App.module.css';
+
+const fetchPosts = async () => (await fetch('/api/posts')).json();
+const addPost = async (text: string) => { await fetch('/api/posts', {
+  method: 'POST',
+  body: JSON.stringify({ text: text }),
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}) };
+
+const App: Component = () => {
+
+  const [posts, setPosts] = createSignal([]);
+  let area: HTMLTextAreaElement | undefined;
+
+
+  const getAll = () => {
+    fetchPosts()
+      .then((value: any) => setPosts(value));
+  };
+
+  const submit = (text: string) => {
+    addPost(text)
+      .then(() => getAll())
+  };
+
+  getAll();
+
+  return (
+    <div class={styles.App}>
+      <textarea ref={area}></textarea>
+      <button onClick={() => submit(area?.value ?? '')}>submit</button>
+
+      <For each={posts()}>{(item: any) => 
+        <div>{item}</div>
+      }</For>
+    </div>
+  );
+};
+
+export default App;
